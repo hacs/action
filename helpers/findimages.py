a24bed11b96f977ea85d455f83c19f73bd3eebc9
Ludeeus <ludeeus@ludeeus.dev>
2020-11-01 15:15:16 +0000

    more output

--- helpers/findimages.py
+++ helpers/findimages.py
@@ -1,8 +1,5 @@
 import sys
 
-print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", str(sys.argv))
-
 with open(sys.argv[-1], "r") as inputfile:
     for line in inputfile.read().split("\n"):
         if "<img" in line or "![" in line:
