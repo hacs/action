249abd68d3d6d1ca6b7bab3e302d81083c599ea8
Ludeeus <ludeeus@ludeeus.dev>
2020-12-22 14:55:55 +0000

    ignore more

--- helpers/findimages.py
+++ helpers/findimages.py
@@ -1,6 +1,17 @@
 import os
 
+ignored=[
+    "-shield",
+    "img.shields.io"
+    "buymeacoffee.com"
+]
+
 with open(f"{os.getenv('GITHUB_ACTION_PATH')}/data/readme.md", "r") as inputfile:
     for line in inputfile.read().split("\n"):
-        if "<img" in line or "![" in line and not "img.shields.io" in line:
-            print(line)
+        if "<img" in line or "![" in line:
+            skip = False
+            for ignore in ignored:
+                if ignore in line:
+                    skip = True
+            if not skip:
+                print(line)
