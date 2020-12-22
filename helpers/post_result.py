cc2cd3a0621aa0c6c0e062d5b9eb5fe10366990f
Ludeeus <ludeeus@ludeeus.dev>
2020-12-22 16:28:44 +0000

    teet

--- helpers/post_result.py
+++ helpers/post_result.py
@@ -3,11 +3,16 @@ import os
 from aiogithubapi import GitHub
 
 def get_token():
-  with open(f"{os.getenv('GITHUB_ACTION_PATH')}/data/token", "r") as token:
-    return token.read()
+    with open(f"{os.getenv('GITHUB_ACTION_PATH')}/data/token", "r") as token:
+        return token.read()
+
+def get_event():
+    with open(os.getenv('GITHUB_EVENT_PATH'), "r") as event:
+        print(event.read())
 
 
 async def post():
+    get_event()
     async with GitHub(get_token()) as github:
         repository = await github.get_repo("ludeeus/aiogithubapi")
         print("Repository description:", repository.full_name)
