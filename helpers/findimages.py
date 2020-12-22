f38fe5a026376f39590c86909ae27939d1c8711f
Ludeeus <ludeeus@ludeeus.dev>
2020-12-22 14:51:36 +0000

    ignore img.shields.io

--- helpers/findimages.py
+++ helpers/findimages.py
@@ -2,5 +2,5 @@ import os
 
 with open(f"{os.getenv('GITHUB_ACTION_PATH')}/data/readme.md", "r") as inputfile:
     for line in inputfile.read().split("\n"):
-        if "<img" in line or "![" in line:
+        if "<img" in line or "![" in line and not "img.shields.io" in line:
             print(line)
