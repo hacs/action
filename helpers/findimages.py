1630d8600534d98d25e6bc896e3deb0ce841d3db
Ludeeus <ludeeus@ludeeus.dev>
2020-12-22 14:49:54 +0000

    fix find images

--- helpers/findimages.py
+++ helpers/findimages.py
@@ -1,6 +1,6 @@
-import sys
+import os
 
-with open(sys.argv[-1], "r") as inputfile:
+with open(f"{os.getenv('GITHUB_ACTION_PATH')}/data/readme.md", "r") as inputfile:
     for line in inputfile.read().split("\n"):
         if "<img" in line or "![" in line:
             print(line)
