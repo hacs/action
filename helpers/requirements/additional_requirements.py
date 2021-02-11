031cfa32666d57b39d47bd3e790ef5736547c033
Ludeeus <ludeeus@ludeeus.dev>
2021-02-11 13:52:18 +0000

    only add if not present

--- helpers/requirements/additional_requirements.py
+++ helpers/requirements/additional_requirements.py
@@ -6,7 +6,8 @@ with open("./requirements.json") as requirements_file:
 with open("./additional_requirements.json") as requirements_file:
   additional_requirements = json.loads(requirements_file.read())
   for req in additional_requirements["apk"]:
-    requirements["apk"].append(req)
+    if req not in requirements["apk"]:
+      requirements["apk"].append(req)
 
 with open("./requirements.json", "w") as requirements_file:
   requirements_file.write(json.dumps(requirements))
