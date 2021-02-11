3f10adfce8b32c40495ab103d5ca747dcce053e3
Ludeeus <ludeeus@ludeeus.dev>
2021-02-11 13:47:58 +0000

    loads

--- helpers/requirements/additional_requirements.py
+++ helpers/requirements/additional_requirements.py
@@ -1,10 +1,10 @@
 import json
 
 with open("./requirements.json") as requirements_file:
-  requirements = json.load(requirements_file.read())
+  requirements = json.loads(requirements_file.read())
 
 with open("./additional_requirements.json") as requirements_file:
-  additional_requirements = json.load(requirements_file.read())
+  additional_requirements = json.loads(requirements_file.read())
   for req in additional_requirements["apk"]:
     requirements["apk"].append(req)
 
